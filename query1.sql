2cc806be931a4d03ba0410b5b09c564c2858506c
Calvin Keith <nivlac123@gmail.com>
2015-11-23 19:59:44 -0800

    updated tables

--- query1.sql
+++ query1.sql
@@ -2,10 +2,10 @@ use RReservation;
 
 CREATE TABLE Restaurant(id SERIAL PRIMARY KEY, name VARCHAR(256));
 
-CREATE TABLE User(id SERIAL PRIMARY KEY, restaurant_id INTEGER, is_manager BOOLEAN);
+CREATE TABLE RRUser(id SERIAL PRIMARY KEY, restaurant_id INTEGER, is_manager BOOLEAN);
 
 CREATE TABLE Reservation(id SERIAL PRIMARY KEY, date TIMESTAMP, restaurant_id INTEGER, customer_id INTEGER, party_count INTEGER);
 
-CREATE TABLE ReservationArchive(id SERIAL PRIMARY, date TIMESTAMP, resteraunt_id INTEGER, customer_id INTEGER, party_count INTEGER);
+CREATE TABLE ReservationArchive(id SERIAL PRIMARY KEY, date TIMESTAMP, restaurant_id INTEGER, customer_id INTEGER, party_count INTEGER);
 
 CREATE TABLE Customer(id SERIAL PRIMARY KEY, reservation_id INTEGER, name VARCHAR(256), phone_number VARCHAR(10), birthdate DATE);
