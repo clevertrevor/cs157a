1d3d64908a76d04ae66dfb9f66db7221a1072b45
Trevor <trevornemanic@gmail.com>
2015-11-23 23:11:56 -0800

    added dropping of tables

--- query1.sql
+++ query1.sql
@@ -1,5 +1,13 @@
 use RReservation;
 
+DROP TABLE CUSTOMER;
+DROP TABLE RESTAURANT;
+DROP TABLE Manager;
+DROP TABLE Reservation;
+DROP TABLE ReservationArchive;
+
+CREATE TABLE Customer(id SERIAL PRIMARY KEY, username VARCHAR(256), login_password VARCHAR(256), reservation_id INTEGER, my_name VARCHAR(256), phone_number VARCHAR(20));
+
 CREATE TABLE Restaurant(id SERIAL PRIMARY KEY, name VARCHAR(256));
 
 CREATE TABLE Manager(id SERIAL PRIMARY KEY, my_name VARCHAR(256), username VARCHAR(256), login_password VARCHAR(256), restaurant_id INTEGER);
@@ -8,4 +16,3 @@ CREATE TABLE Reservation(id SERIAL PRIMARY KEY, reservation_timestamp TIMESTAMP,
 
 CREATE TABLE ReservationArchive(id SERIAL PRIMARY KEY, reservation_timestamp TIMESTAMP, restaurant_id INTEGER, customer_id INTEGER, party_count INTEGER);
 
-CREATE TABLE Customer(id SERIAL PRIMARY KEY, username VARCHAR(256),login_password VARCHAR(256), reservation_id INTEGER, my_name VARCHAR(256), phone_number VARCHAR(20));
