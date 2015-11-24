f11967b8d6650d71f09f96578f80efa49e2a7c07
Trevor <trevornemanic@gmail.com>
2015-11-23 21:44:41 -0800

    RRUser -> Manager

--- query1.sql
+++ query1.sql
@@ -2,11 +2,9 @@ use RReservation;
 
 CREATE TABLE Restaurant(id SERIAL PRIMARY KEY, name VARCHAR(256));
 
-CREATE TABLE RRUser(id SERIAL PRIMARY KEY, restaurant_id INTEGER, is_manager BOOLEAN);
+CREATE TABLE Manager(id SERIAL PRIMARY KEY, restaurant_id INTEGER);
 
 CREATE TABLE Reservation(id SERIAL PRIMARY KEY, reservation_timestamp TIMESTAMP, restaurant_id INTEGER, customer_id INTEGER, party_count INTEGER);
-
-CREATE TABLE ReservationArchive(id SERIAL PRIMARY KEY, date TIMESTAMP, restaurant_id INTEGER, customer_id INTEGER, party_count INTEGER);
 
 CREATE TABLE ReservationArchive(id SERIAL PRIMARY KEY, reservation_timestamp TIMESTAMP, restaurant_id INTEGER, customer_id INTEGER, party_count INTEGER);
 
